91da5f95d84a20d542c38cc3de7c99b995f53418
Alan de Jesus lima <alanjl29@gmail.com>
2021-12-28 11:50:22 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// Efeito de foco no input
+const textInputs = document.querySelectorAll("input");
+
+textInputs.forEach(textInput =>{
+    textInput.addEventListener("focus", () => {
+        let parent = textInput.parentNode;
+        parent.classList.add("active");
+    });
+});
+
+
+textInputs.forEach(textInput =>{
+    textInput.addEventListener("blur", () => {
+        let parent = textInput.parentNode;
+        parent.classList.remove("active");
+    });
+});
+
+
+// Mostrar/Ocultar senha
+
+const passwordInput = document.querySelector(".password-input");
+const eyeBtn = document.querySelector(".eye-btn");
+
+eyeBtn.addEventListener("click", () => {
+    if(passwordInput.type === "password"){
+        passwordInput.type = "text";
+        eyeBtn.innerHTML = "<i class='uil uil-eye'></i>"
+    }
+    else{
+        passwordInput.type = "password";
+        eyeBtn.innerHTML = "<i class='uil uil-eye-slash'></i>"
+    }
+});
+
+// Slider entre os formulários
+
+const signUpBtn = document.querySelector(".sign-up-btn");
+const signInBtn = document.querySelector(".sign-in-btn");
+const signUpForm = document.querySelector(".sign-up-form");
+const signInForm = document.querySelector(".sign-in-form");
+
+
+signUpBtn.addEventListener("click", () => {
+    signInForm.classList.add("hide");
+    signUpForm.classList.add("show");
+    signInForm.classList.remove("show");
+});
+
+signInBtn.addEventListener("click", () => {
+    signInForm.classList.remove("hide");
+    signUpForm.classList.remove("show");
+    signInForm.classList.add("show");
+});
